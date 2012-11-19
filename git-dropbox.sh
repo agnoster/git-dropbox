da77d12e7ca03e71caf11e4bccd9ce549edbed1d
Skye Book <skye.book@gmail.com>
2012-11-19 02:08:36 -0500

    Fix for projects with spaces in name
    Variables need to be quoted in order for spaces to be picked up correctly.

--- git-dropbox.sh
+++ git-dropbox.sh
@@ -42,8 +42,8 @@ if [ ! "$DROPBOX_REPO" ]; then
 fi
 
 if [ ! -d "$DROPBOX_REPO" ]; then
-  mkdir -p $DROPBOX_REPO
-  (cd $DROPBOX_REPO; git init --bare)
+  mkdir -p "$DROPBOX_REPO"
+  (cd "$DROPBOX_REPO"; git init --bare)
 fi
 
 if [ ! -d "$DROPBOX_REPO" ]; then
